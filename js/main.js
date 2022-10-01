cbd1a8a886a9f5c18251e80e79c9c94036cecbb2
Damian <diqqqtv@gmail.comgit config --global user.email damian.mariusz.pyszka@gmail.comgit config --global user.name Damiangit commit -m created>
2022-10-01 16:41:03 +0200

    Created massages and test sections

--- js/main.js
+++ js/main.js
@@ -8,6 +8,35 @@ const addShadow = () => {
     }
 }
 
-console.log(nav.offsetTop);
-
 window.addEventListener('scroll', addShadow)
+
+
+
+
+
+
+
+
+
+
+
+// test
+const cardFront = document.querySelector('.front-card')
+const cardBack = document.querySelector('.back-card')
+
+const cardRotate = () => {
+    if (cardFront.classList.contains('show')) {
+        cardFront.classList.remove('show')
+        cardFront.classList.add('hide')
+        cardBack.classList.remove('hide')
+        cardBack.classList.add('show')
+    } else {
+        cardFront.classList.remove('hide')
+        cardFront.classList.add('show')
+        cardBack.classList.remove('show')
+        cardBack.classList.add('hide')
+    }
+}
+
+cardFront.addEventListener('click', cardRotate)
+cardBack.addEventListener('click', cardRotate)
